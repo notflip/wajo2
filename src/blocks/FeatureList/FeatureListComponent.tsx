6b3ec4cb2ddf410680e614cbdeddfdd1e4e3570b
Miguel Stevens <miguel@studiomonty.be>
2025-04-03 11:51:32 +0200

    FeatureListComponent

--- src/blocks/FeatureList/FeatureListComponent.tsx
+++ src/blocks/FeatureList/FeatureListComponent.tsx
@@ -12,8 +12,8 @@ export const FeatureListComponent: React.FC<FeatureList> = async (props) => {
     <section className="relative pt-[3rem] pb-[3rem]">
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8 2xl:px-16">
         <Badge text={badge} />
-        <div className="mt-8 flex items-center justify-between">
-          <h2 className="max-w-3xl">{title}</h2>
+        <div className="mt-8 flex flex-col gap-8 md:flex-row md:items-center md:justify-between">
+          <h2 className="max-w-4xl">{title}</h2>
           <AnimatedButton variant="light" asChild>
             <CmsLink {...link} />
           </AnimatedButton>
@@ -23,16 +23,19 @@ export const FeatureListComponent: React.FC<FeatureList> = async (props) => {
             <CmsLink
               {...item.link}
               key={index}
-              className="group flex items-center mb-4 justify-between bg-blue-50 transition hover:bg-primary p-8 rounded-[16px]"
+              className="group flex flex-col md:grid md:grid-cols-[minmax(0,1fr)_1fr_auto] gap-4 md:items-center mb-4 bg-blue-50 transition hover:bg-primary p-8 rounded-[16px]"
             >
-              <h4 key={index} className="text-foreground group-hover:text-white">
+              <h4 className="text-foreground group-hover:text-white md:mb-0 mb-2">
                 {item.title}
               </h4>
-              <p className="text-foreground group-hover:text-white">
+              <p className="max-w-md text-foreground group-hover:text-white md:mb-0 mb-4">
                 {item.text}
               </p>
-              <div className="text-foreground group-hover:text-white">
-                <HiOutlineArrowUpRight size="24" className="group-hover:rotate-45 transition" />
+              <div className="text-foreground group-hover:text-white md:ml-auto ml-0">
+                <HiOutlineArrowUpRight
+                  size="24"
+                  className="group-hover:rotate-45 transition"
+                />
               </div>
             </CmsLink>
           ))}
