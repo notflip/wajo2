2279d041935129f712c1cebc77d239df8831f61c
Miguel Stevens <miguel@studiomonty.be>
2025-04-11 14:00:06 +0200

    Fixed issue with CmsLink

--- src/blocks/Cards/CardsComponent.tsx
+++ src/blocks/Cards/CardsComponent.tsx
@@ -19,14 +19,19 @@ export const CardsComponent: React.FC<Cards> = (props) => {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           {(items || []).map((item, index) => (
             <CmsLink
-              {...item.link}
+              type="reference"
+              reference={item.reference}
               key={index}
               className="bg-white group p-8 rounded-[16px] hover:-translate-y-1 transition"
             >
               <div>
                 {item.icon && (
                   <div className="inline-flex rounded-full mb-8 p-4 bg-secondary">
-                    <DynamicIcon iconName={item.icon} size={32} className="transition group-hover:rotate-12" />
+                    <DynamicIcon
+                      iconName={item.icon}
+                      size={32}
+                      className="transition group-hover:rotate-12"
+                    />
                   </div>
                 )}
                 <h4 className="mb-8">{item.title}</h4>
