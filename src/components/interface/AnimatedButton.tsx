189609e1ca96d5c567ef08a3d8d591e4aa4a4641
Miguel Stevens <miguel@studiomonty.be>
2025-04-29 14:43:08 +0200

    AnimatedButton server

--- src/components/interface/AnimatedButton.tsx
+++ src/components/interface/AnimatedButton.tsx
@@ -1,6 +1,4 @@
-"use client"
-
-import { cva, type VariantProps } from "class-variance-authority"
+import { cva } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 import { ButtonHTMLAttributes, cloneElement, isValidElement, ReactNode } from "react"
 
